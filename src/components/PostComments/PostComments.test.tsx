6ec7f6df37bab03750e5b4baaddf7f560c78849d
Marcel <uehdev@gmail.com>
2023-12-27 20:50:03 -0300

    Commit de entrega do exercicio

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,40 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+const comentarios = [
+  "Que foto legal!",
+  "Esse é o meu segundo comentário! Lorem ipsum dolor sit amte consectur?!",
+];
+
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve renderizar 2 novos comentarios publicados pelos usuário", () => {
+    const { debug } = render(<PostComment />);
+
+    //Adiciona primeiro comentario
+    fireEvent.change(screen.getByTestId("caixa-comentario"), {
+      target: {
+        value: comentarios[0],
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comentar"));
+    //Checa se o primeiro comentario foi renderizado
+    expect(screen.getByText(comentarios[0])).toBeInTheDocument();
+
+    //Adiciona segundo comentario
+    fireEvent.change(screen.getByTestId("caixa-comentario"), {
+      target: {
+        value: comentarios[1],
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comentar"));
+    debug();
+    //Checa se o segundo comentario foi renderizado
+    expect(screen.getByText(comentarios[1])).toBeInTheDocument();
+  });
 });
